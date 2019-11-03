ec07d60a1f1d5395c5422392f4f059dbb8937c3d
Michael Weimann <mail@michael-weimann.eu>
2019-11-03 15:32:08 +0100

    Add city/country

--- generator/index.py
+++ generator/index.py
@@ -2,6 +2,33 @@ import csv
 from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
 
+iso_label_dict = {
+    'AT': 'Austria',
+    'BE': 'Belgium',
+    'BG': 'Bulgaria',
+    'BY': 'Belarus',
+    'CH': 'Switzerland',
+    'CZ': 'Czech Republic',
+    'DE': 'Germany',
+    'DK': 'Denmark',
+    'ES': 'Spain',
+    'FR': 'France',
+    'GR': 'Greece',
+    'HR': 'Croatia',
+    'HU': 'Hungary',
+    'IT': 'Italy',
+    'KO': 'Kosova',
+    'NL': 'the Netherlands',
+    'NO': 'Norway',
+    'PL': 'Poland',
+    'PT': 'Portugal',
+    'RO': 'Romania',
+    'RS': 'Serbia',
+    'SE': 'Sweden',
+    'SQ': 'Albania',
+    'UK': 'UK'
+}
+
 months = {
     '01': {
         'label': 'January',
@@ -56,19 +83,37 @@ months = {
 with open('2019_events_db.csv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter='\t')
     for row in reader:
-        date_start = datetime.strptime(row['datestart'], '%Y%m%d')
-        day_start = date_start.strftime('%d')
-        month_start = date_start.strftime('%m')
-        print('start: ' + month_start)
+        start_date = datetime.strptime(row['datestart'], '%Y%m%d')
+        start_day = start_date.strftime('%d')
+        start_month = start_date.strftime('%m')
+
+        end_date = datetime.strptime(row['dateend'], '%Y%m%d')
+        end_day = end_date.strftime('%d')
+
+        if row['city'] == '--' or not row['city']:
+            city = ''
+        else:
+            city = row['city']
+
+        if row['country'] == '--' or not row['country']:
+            country = ''
+        else:
+            country_code = row['country'].strip()
+            country = iso_label_dict.get(country_code, country_code)
+
         event = {
-            "label": row['label'],
-            "start_day": day_start
+            'label': row['label'],
+            'start_day': start_day,
+            'end_day': end_day,
+            'homepage': row['homepage'],
+            'city': city,
+            'country': country
         }
-        months[month_start]['events'].append(event)
+        months[start_month]['events'].append(event)
 
 file_loader = FileSystemLoader('src/templates')
 env = Environment(loader=file_loader)
 template = env.get_template('index.html')
 result = template.render(months=months)
-with open("build/index.html", "a") as f:
+with open('build/index.html', 'a') as f:
     f.write(result)
