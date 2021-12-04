0a20ee245d3377291d20ec96d296b9c88d8f82ef
Michael Weimann <mail@michael-weimann.eu>
2021-12-04 18:43:03 +0100

    add 2022 event generator

--- generator/event_pages.py
+++ generator/event_pages.py
@@ -24,6 +24,6 @@ def generate_event_pages(events, all_events):
 all_events = parse_all_events()
 today = datetime.now()
 
-for year in [2019, 2020, 2021]:
+for year in [2019, 2020, 2021, 2022]:
     events = parse_events('data/' + str(year) + '_events_db.csv', today)
     generate_event_pages(events['all'].values(), all_events)
